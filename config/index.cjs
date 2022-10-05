095366e90705c31bfce495c9e44ff57b97cf281b
Clown-77 <48898447+Clown-77@users.noreply.github.com>
2022-10-05 20:29:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'b3771781f3ed96fd6bb29dace134cc39',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '安徽省',
+  CITY: '合肥',
 
   USERS: [
     {
