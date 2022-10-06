ca56ea0cf538c0a239b926d696a603d711099866
Clown-77 <48898447+Clown-77@users.noreply.github.com>
2022-10-06 10:41:49 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '清宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oQd_p52JLqy5BkmbdS-jF7aGaxss',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -29,14 +29,14 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1999', date: '07-04',
+          type: '*生日', name: '清宝', year: '1999', date: '07-04',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1999', date: '05-21',
+          type: '生日', name: '清宝', year: '1999', date: '05-21',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '09-16',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
