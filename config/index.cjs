d0021fd75ac56ae2cfa307d576d03114845b2b39
Clown-77 <48898447+Clown-77@users.noreply.github.com>
2022-12-21 09:39:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝，我错了',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oQd_p5yIQ7LiFDIVKG_1bjVXMitY',
+      id: 'oQd_p52JLqy5BkmbdS-jF7aGaxss',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '27nQYUl6SF0PckcDvYOVTEk9VOnmn4HiAqNwt86f5zI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
