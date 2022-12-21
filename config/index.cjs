da095535d07e94bad37fd3827f95f3299483d1ce
Clown-77 <48898447+Clown-77@users.noreply.github.com>
2022-12-21 10:08:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oQd_p52JLqy5BkmbdS-jF7aGaxss',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'sLx45FC8C9bG9lwcbwU8iqnFHhiDEyInzMhHNtMpk7o',
+      useTemplateId: '27nQYUl6SF0PckcDvYOVTEk9VOnmn4HiAqNwt86f5zI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-21',
       festivals: [
