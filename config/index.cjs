257efc5ef596586f3e516944058bc5d9381f237a
Clown-77 <48898447+Clown-77@users.noreply.github.com>
2022-12-21 09:41:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -39,8 +39,7 @@ const USER_CONFIG = {
           type: '节日', name: '相识纪念日', year: '2022', date: '09-16',
         },
       ],
-      清清，我真的好想你，回来吧，我们不要冷战好吗。
-      // 我们在一起已经有xxxx天了的配置
+        // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2022-09-16' },
