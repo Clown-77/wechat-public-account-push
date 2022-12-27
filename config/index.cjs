5bf00da577131d9d47287ac0ff92b5235419ead8
Clown-77 <48898447+Clown-77@users.noreply.github.com>
2022-12-27 23:16:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '清宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oQd_p5yIQ7LiFDIVKG_1bjVXMitY',
+      id: 'oQd_p5yIQ7LiFDIVKG_1bjVXMitY失恋',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'X4NVNv5rdPgZcZRPJRo0bWO6DcR3dXbY0EluL9bgScw',
+      useTemplateId: 'X4NVNv5rdPgZcZRPJRo0bWO6DcR3dXbY0EluL9bgScw失恋',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-21',
       festivals: [
@@ -59,7 +59,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oQd_p52JLqy5BkmbdS-jF7aGaxss',
+      id: 'oQd_p52JLqy5BkmbdS-jF7aGaxss失恋',
     }
   ],
 
